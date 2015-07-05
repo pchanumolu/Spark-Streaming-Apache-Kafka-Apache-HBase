c022555f0107e010cf168345216abdfdc3ddf136
Pradeep Chanumolu <pchanumolu@maprtech.com>
2015-07-04 23:55:14 -0700

    Checking in whole project at once

---
+++ src/main/scala/sparkExample/SparkKafkaHBase.java
@@ -0,0 +1,110 @@
+package sparkExample
+
+import kafka.serializer.{DefaultDecoder, StringDecoder}
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.hbase.HBaseConfiguration
+import org.apache.hadoop.hbase.client.{HTableInterface, HConnectionManager, HConnection, Put}
+import org.apache.hadoop.hbase.mapreduce.TableOutputFormat
+import org.apache.hadoop.hbase.util.Bytes
+import org.apache.hadoop.io.{LongWritable, Text}
+import org.apache.spark.SparkConf
+import org.apache.spark.rdd.PairRDDFunctions
+import org.apache.spark.storage.StorageLevel
+import org.apache.spark.streaming.kafka.{HasOffsetRanges, KafkaUtils}
+import org.apache.spark.streaming.{Minutes, Seconds, StreamingContext}
+
+
+/**
+ * Created by pchanumolu on 06/06/15.
+ */
+object SparkKafkaHBase {
+
+  def main(args: Array[String]): Unit = {
+    if (args.length < 2) {
+      System.err.println("Usage: <broker-list> <zk-list> <topic>")
+      System.exit(1)
+    }
+
+    val Array(broker, zk, topic) = args
+
+    val sparkConf = new SparkConf().setAppName("KafkaSparkStreamingHBaseWordCount")
+    val ssc = new StreamingContext(sparkConf, Seconds(10))
+    ssc.checkpoint("maprfs:///sparkStreamingCheckpoints")
+
+    // configuration to connect to kafka cluster
+    val kafkaConf = Map("metadata.broker.list" -> broker,
+      "zookeeper.connect" -> zk,
+      "group.id" -> "kafka-spark-streaming-example",
+      "zookeeper.connection.timeout.ms" -> "1000")
+
+    // Kafka integration with reciever
+    val lines = KafkaUtils.createStream[Array[Byte], String, DefaultDecoder, StringDecoder](
+      ssc, kafkaConf, Map(topic -> 1),
+      StorageLevel.MEMORY_ONLY_SER).map(_._2)
+
+
+    // Getting Kafka offsets from RDDs
+    lines.foreachRDD { rdd =>
+      val offsetRanges = rdd.asInstanceOf[HasOffsetRanges].offsetRanges
+      offsetRanges.foreach(println(_))
+    }
+
+    val words = lines.flatMap(_.split(" "))
+
+    val wordCounts = words.map(x => (x, 1L)).reduceByKey(_ + _).reduceByKeyAndWindow(_ + _, _ - _, Minutes(5), Seconds(2), 2)
+    wordCounts.print()
+
+    wordCounts.foreachRDD(rdd => {
+
+      val conf = getHBaseConfig()
+      val jobConf = new Configuration(conf)
+      jobConf.set("mapreduce.job.output.key.class", classOf[Text].getName)
+      jobConf.set("mapreduce.job.output.value.class", classOf[LongWritable].getName)
+      jobConf.set("mapreduce.outputformat.class", classOf[TableOutputFormat[Text]].getName)
+
+      rdd.saveAsNewAPIHadoopDataset(jobConf)
+
+      rdd.saveAsTextFile("maprfs:///sparkstreaming-kafka-output")
+      new PairRDDFunctions(rdd.map(convert)).saveAsNewAPIHadoopDataset(jobConf)
+      rdd.foreach({
+                    case (value, count) => {
+                      println("value --> " + value + " with count --> " + count)
+                    }
+                  })
+      val connection = connect("stream_count")
+      rdd.foreach( record => connection.put(putRequest(record)) )
+    })
+
+    wordCounts.print()
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+
+
+  def getHBaseConfig() : Configuration = {
+    val conf = HBaseConfiguration.create()
+    conf.set(TableOutputFormat.OUTPUT_TABLE, "stream_count")
+    conf.set("hbase.zookeeper.quorum", "localhost")
+    conf.set("hbase.zookeeper.property.clientPort","5181")
+    conf
+  }
+
+  def connect(tableName:String) : HTableInterface = {
+    val conf = getHBaseConfig()
+    val hCon = new HConnectionManager.createConnection(conf)
+    val connection = hCon.getTable(tableName)
+    connection
+  }
+  def putRequest(t: (String, Long)) = {
+    val p = new Put(Bytes.toBytes(t._1))
+    p.add(Bytes.toBytes("word"), Bytes.toBytes("count"), Bytes.toBytes(t._2))
+  }
+
+  def convert(t: (String, Long)) = {
+    val p = new Put(Bytes.toBytes(t._1))
+    p.add(Bytes.toBytes("word"), Bytes.toBytes("count"), Bytes.toBytes(t._2))
+    (t._1, p)
+  }
+}
+
